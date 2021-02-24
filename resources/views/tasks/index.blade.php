90383cbbdd92c479b95c9f3b1eaedc21000e2d80
maita.hbk <maitahbk@icloud.com>
2021-02-24 14:49:57 +0000

    second commit

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
 
-    <h1>メッセージ一覧</h1>
+    <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
         <table class="table table-striped">
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>メッセージ</th>
+                    <th>タスク</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($tasks as $task)
                 <tr>
-                    {{-- メッセージ詳細ページへのリンク --}}
+                    {{-- タスク詳細ページへのリンク --}}
                     <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
                 </tr>
@@ -26,7 +26,7 @@
     @endif
     
 
-    {{-- メッセージ作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規メッセージの投稿', [], ['class' => 'btn btn-primary']) !!}
+    {{-- タスク作成ページへのリンク --}}
+    {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
